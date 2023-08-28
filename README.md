05d546bee61e59de3bae3932e2bdc43cf35a98c9
Martin Hanes <martin.hanes@gmail.com>
2023-08-28 20:52:47 +0200

    added support for time units, reporting waiting state when timeout is active to the UI, reporting timeout setting as node name and updated UI for editing node.
    Signed-off-by: Martin Hanes <martin.hanes@gmail.com>

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-# node-red-contrib-debounce
+# Node-RED Debounce
 
 ```
-npm install node-red-contrib-debounce
+npm install node-red-contrib-debounce-leading-trailing
 ```
 
-A node implementing the same logic as [the Rx debounce operator](http://reactivex.io/documentation/operators/debounce.html).
+A node for Node-RED that supports leading (immediate) or trailing (delayed) debounce logic.
+
+Trailing (delayed): wait first, then fire.
+
+Leading (immediate): fire first, then act as trailing.
+
+Forked from telmomarques / node-red-contrib-debounce-leading-trailing, added support for time units, reporting waiting state, and updated UI.
--- nodes/debounce.js
+++ nodes/debounce.js
@@ -1,25 +1,54 @@
 module.exports = function(RED) {
-  function DebounceNode(config) {
+    function DebounceLeadingTrailingNode(config) {
 
-    RED.nodes.createNode(this, config);
-    this.time = Math.max(config.time || 1000, 0);
-    this.name = config.name;
+        RED.nodes.createNode(this, config);
+        this.timeunit = config.timeunit || 's';
+        this.timeInput = Math.max(config.time || 5, 0);
+        switch (this.timeunit) {
+            case 's':
+                this.time = this.timeInput * 1000;
+                break;
+            case 'm':
+                this.time = this.timeInput * 1000 * 60;
+                break;
+            default: //ms
+                this.time = this.timeInput;
+                break;
+        }
 
-    var context = this.context();
-    var node = this;
-    this.on('input', function(msg) {
-      var oldTimeout = context.get('timeout');
-      if (oldTimeout) {
-        clearTimeout(oldTimeout);
-      }
+        this.name = config.name; // + " " + this.timeInput + " " + this.timeunit;
+        this.debouncetype = config.debouncetype || "leading";
 
-      var timeout = setTimeout(function() {
-        node.send(msg);
-      }, node.time);
+        let context = this.context();
+        let node = this;
 
-      context.set('timeout', timeout);
+        this.on('input', function (msg) {
+            let oldTimeout = context.get('timeout');
+            let lastTimestamp = context.get('timestamp');
+            let currentTimestamp = Date.now();
+            let interval = currentTimestamp - lastTimestamp || node.time;
 
-    });
-  }
-  RED.nodes.registerType('debounce', DebounceNode);
+            if (oldTimeout) {
+                clearTimeout(oldTimeout);
+            }
+
+            if (node.debouncetype === "trailing" || interval < node.time) {
+                let timeout = setTimeout(function () {
+                    node.send(msg);
+                    node.status({text: ""})
+                }, node.time);
+
+                node.status({text: "waiting (" + node.debouncetype + ")"})
+                context.set('timeout', timeout);
+            }
+
+            if (node.debouncetype === "leading" && interval >= node.time) {
+                node.send(msg);
+                node.status({text: ""})
+                context.set('timestamp', currentTimestamp);
+            }
+        });
+    }
+
+    RED.nodes.registerType('debounce-leading-trailing', DebounceLeadingTrailingNode);
 };
