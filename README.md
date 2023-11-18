9942b9ce076d317d16402fdac4ad2a2c259fb4c2
Martin Hanes <martin.hanes@gmail.com>
2023-11-18 10:07:02 +0100

    added flush and reset capabilities, providing richer status message
    Signed-off-by: Martin Hanes <martin.hanes@gmail.com>

--- README.md
+++ README.md
@@ -10,4 +10,7 @@ Trailing (delayed): wait first, then fire.
 
 Leading (immediate): fire first, then act as trailing.
 
-Forked from telmomarques / node-red-contrib-debounce-leading-trailing, added support for time units, reporting waiting state, and updated UI.
+Forked from telmomarques / node-red-contrib-debounce-leading-trailing, added support for time units, reporting all
+states (waiting, last fired time, reset/flush) and updated UI.
+
+v0.0.7 - added flush and reset capabilities to either flush the message immediately or reset the node.
--- nodes/debounce.js
+++ nodes/debounce.js
@@ -19,32 +19,74 @@ module.exports = function(RED) {
         this.name = config.name; // + " " + this.timeInput + " " + this.timeunit;
         this.debouncetype = config.debouncetype || "leading";
 
+        const STATUS_TIMEOUT = 10000;
         let context = this.context();
         let node = this;
 
+        function setStatusMessageTemp(fill, text) {
+            let oldTimeout = context.get('statusTimeout');
+            if (oldTimeout) {
+                clearTimeout(oldTimeout)
+            }
+
+            node.status({fill: fill, text: text})
+
+            const statusTimeout = setTimeout(function () {
+                node.status({})
+            }, STATUS_TIMEOUT);
+
+            context.set('statusTimeout', statusTimeout);
+        }
+
+        function formatDate(input) {
+            return input.toLocaleString('en-US', {
+                month: 'short',
+                day: 'numeric',
+                hour: 'numeric',
+                minute: 'numeric',
+                hour12: false
+            });
+        }
+
         this.on('input', function (msg) {
+
             let oldTimeout = context.get('timeout');
             let lastTimestamp = context.get('timestamp');
             let currentTimestamp = Date.now();
             let interval = currentTimestamp - lastTimestamp || node.time;
 
+            // we do clear old timer in all cases - either flush, reset or trailing
+            // for leading - we don't have timeout
             if (oldTimeout) {
                 clearTimeout(oldTimeout);
             }
 
+            if (msg.hasOwnProperty("flush")) {
+                setStatusMessageTemp("green", "flushed at: " + formatDate(new Date()))
+                node.send(msg);
+                return;
+            }
+
+            if (msg.hasOwnProperty("reset")) {
+                // node.status({text: ""})
+                setStatusMessageTemp("red", "reset at: " + formatDate(new Date()))
+                return;
+            }
+
+            // in case we should wait
             if (node.debouncetype === "trailing" || interval < node.time) {
                 let timeout = setTimeout(function () {
+                    node.status({fill: "green", text: formatDate(new Date())})
                     node.send(msg);
-                    node.status({text: ""})
                 }, node.time);
 
-                node.status({text: "waiting (" + node.debouncetype + ")"})
+                node.status({fill: "blue", text: "waiting (" + node.debouncetype + ")"})
                 context.set('timeout', timeout);
             }
 
             if (node.debouncetype === "leading" && interval >= node.time) {
                 node.send(msg);
-                node.status({text: ""})
+                node.status({fill: "green", text: formatDate(new Date())})
                 context.set('timestamp', currentTimestamp);
             }
         });
